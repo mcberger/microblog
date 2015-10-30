109ed32fd0950e5873c69ba17d781f0b40d88ad6
Renaldas Zlatkus <renaldas.zlatkus@gmail.com>
2015-10-30 16:00:04 +0100

    added a route from home.erb to app.rb

--- app.rb
+++ app.rb
@@ -8,3 +8,19 @@ enable :sessions
 use Rack::Flash, :sweep =>true
 
 set :database, "sqlite3:Micrapost_db.sqlite3"
+
+
+# post for home login
+post '/login' do
+	puts "my params are" + params.inspect
+	user = User.where(username: params[:username]).first
+	if user && user.password == params[:password]
+		session[:user_id] = user.id
+		flash[:notice] = "You've been signed in successfully."
+		redirect #{profile}"/#"
+	else
+		flash[:alert] = "There was a problem signing you in."
+	end
+	redirect '/login'
+end
+
