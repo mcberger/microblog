0ee2e3b54c73b8cd2674dd60b3322118a13db415
Mai Berger <m.c.berger@icloud.com>
2015-10-30 16:23:11 +0100

    fixed invisible error

--- models.rb
+++ models.rb
@@ -1,13 +1,10 @@
 class User < ActiveRecord::Base
   has_many :follower_follows, foreign_key: :followee_id, class_name: 'Follow'
   has_many :followee_follows, foreign_key: :follower_id, class_name: 'Follow'
-​
   has_many :followers, through: :follower_follows
   has_many :followees, through: :followee_follows 
-
   has_many :posts
 end
-​
 class Follow < ActiveRecord::Base
   belongs_to :follower, class_name: 'User', foreign_key: :follower_id
   belongs_to :followee, class_name: 'User', foreign_key: :followee_id
